5cd4fcfaf007d21f363e1594a0e912e495ff3399
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2023-04-23 12:30:59 +0200

    Full parametrized build

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -4,16 +4,30 @@ IMAGE_NAME=${1:-rutkowski/cod2}
 PUSH=${2:-""}
 
 cod_patches=( 0 2 3 )
-mysql_options=( 1 2 )
+mysql_variants=( 1 2 )
+speex_options=( 0 1 )
+enable_unsafe_options=( 0 1 )
 
 for cod_patch in "${cod_patches[@]}"
 do
-    for mysql_option in "${mysql_options[@]}"
+    for mysql_variant in "${mysql_variants[@]}"
     do
-        echo "Building and pushing with parameters cod_patch=$cod_patch, mysql_option=$mysql_option"
+        for speex in "${speex_options[@]}"
+        do
+            for enable_unsafe in "${enable_unsafe_options[@]}"
+            do
+                echo "Building and pushing with parameters cod_patch=$cod_patch, mysql_variant=$mysql_variant, speex=$speex, enable_unsafe=$enable_unsafe"
 
-        ./build.sh $IMAGE_NAME $cod_patch $mysql_option $PUSH
+                ./build.sh \
+                    --image_name=$IMAGE_NAME \
+                    --cod2_patch=$cod_patch \
+                    --mysql_variant=$mysql_variant \
+                    --speex=$speex \
+                    --enable_unsafe=$enable_unsafe \
+                    --push=$PUSH
 
-        echo "Done"
+                echo "Done"
+            done
+        done
     done
 done
